ab3a5c22a964e26e165cd0af147a4961d7dd0047
¨LambertSchulze¨ <schulze.lambert@googlemail.com>
2020-07-17 01:07:59 +0200

    -minor fixes in html -changed console outputs to dom output -moved game logic to event listener callback

--- script.js
+++ script.js
@@ -8,45 +8,47 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection == computerSelection) {
-        console.log("Tie! No points For both");
+        message.textContent = "Tie! No points For both";
         return "";
     }
     else if (playerSelection   == "rock" &&
-         computerSelection == "paper") {
-        console.log("Computer chose 'Paper': You loose.");
+        computerSelection == "paper") {
+        message.textContent = "Computer chose 'Paper': You loose.";
         return "computer";
     }
     else if (playerSelection   == "rock" &&
-         computerSelection == "scissors") {
-        console.log("Computer chose 'Scissors': This round goes to you!");
+        computerSelection == "scissors") {
+        message.textContent = "Computer chose 'Scissors': This round goes to you!";
         return "player";
     }
     else if (playerSelection   == "paper" &&
-             computerSelection == "rock") {
-        console.log("Computer chose 'Rock': This round goes to you!");
+        computerSelection == "rock") {
+        message.textContent = "Computer chose 'Rock': This round goes to you!";
         return "player";
     }
     else if (playerSelection   == "paper" &&
-         computerSelection == "scissors") {
-        console.log("Computer chose 'Scissors': You loose.");
+        computerSelection == "scissors") {
+        message.textContent = "Computer chose 'Scissors': You loose.";
         return "computer";
     }
     else if (playerSelection   == "scissors" &&
-         computerSelection == "rock") {
-        console.log("Computer chose 'Rock': You loose.");
+        computerSelection == "rock") {
+        message.textContent = "Computer chose 'Rock': You loose.";
         return "computer";
     }
     else if (playerSelection   == "scissors" &&
-         computerSelection == "paper") {
-        console.log("Computer chose 'Paper': This round goes to you!");
+        computerSelection == "paper") {
+        message.textContent = "Computer chose 'Paper': This round goes to you!";
         return "player";
     }
     else {
-        console.log("Error. This state should not happen\n" +
-                "Player: {playerState}\n" +
-                "Computer: {computerState}");
+        message.textContent = "Error. This state should not happen\n";
     }
 }
+const message = document.querySelector('#message');
+const score =   document.querySelector('#score');
+
+score.textContent = "Player-Computer: 0-0";
 
 
 let playerScore   = 0;
@@ -56,18 +58,18 @@ let roundWinner;
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        //alert(button.id);
-    
+    button.addEventListener('click', () => {    
         if      (button.id == 'rock')     roundWinner = playRound('rock', computerSelection);
         else if (button.id == 'paper')    roundWinner = playRound('paper', computerSelection);
         else if (button.id == 'scissors') roundWinner = playRound('scissors', computerSelection);
+
+        if      (roundWinner == "player")   playerScore++;
+        else if (roundWinner == "computer") computerScore++;
+    
+        score.textContent = "Player-Computer: "+playerScore.toString()+"-"+computerScore.toString();
     });
 });
 
 if      (roundWinner == "player")   playerScore++;
 else if (roundWinner == "computer") computerScore++;
 
-
-console.log("You have won {playerScore} Rounds.");
-console.log("The computer won {computerScore} Rounds.");
