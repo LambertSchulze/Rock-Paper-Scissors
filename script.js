476498ef0e52f15bfa517617fb4c68dd65c303b8
¨LambertSchulze¨ <schulze.lambert@googlemail.com>
2020-07-20 01:38:00 +0200

    Added words befor emoji in JS output.

--- script.js
+++ script.js
@@ -13,32 +13,32 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (playerSelection   == "rock" &&
         computerSelection == "paper") {
-        message.textContent = "Computer chose " + String.fromCodePoint(0x270B) + ": You loose.";
+        message.textContent = "Computer chose Paper " + String.fromCodePoint(0x270B) + ": You loose.";
         return "computer";
     }
     else if (playerSelection   == "rock" &&
         computerSelection == "scissors") {
-        message.textContent = "Computer chose " + String.fromCodePoint(0x270C) + ": This round goes to you!";
+        message.textContent = "Computer chose Scissors " + String.fromCodePoint(0x270C) + ": This round goes to you!";
         return "player";
     }
     else if (playerSelection   == "paper" &&
         computerSelection == "rock") {
-        message.textContent = "Computer chose " + String.fromCodePoint(0x270A) + ": This round goes to you!";
+        message.textContent = "Computer chose Rock " + String.fromCodePoint(0x270A) + ": This round goes to you!";
         return "player";
     }
     else if (playerSelection   == "paper" &&
         computerSelection == "scissors") {
-        message.textContent = "Computer chose " + String.fromCodePoint(0x270C) + ": You loose.";
+        message.textContent = "Computer chose Scissors " + String.fromCodePoint(0x270C) + ": You loose.";
         return "computer";
     }
     else if (playerSelection   == "scissors" &&
         computerSelection == "rock") {
-        message.textContent = "Computer chose " + String.fromCodePoint(0x270A) + ": You loose.";
+        message.textContent = "Computer chose Rock " + String.fromCodePoint(0x270A) + ": You loose.";
         return "computer";
     }
     else if (playerSelection   == "scissors" &&
         computerSelection == "paper") {
-        message.textContent = "Computer chose " + String.fromCodePoint(0x270B) + ": This round goes to you!";
+        message.textContent = "Computer chose Paper " + String.fromCodePoint(0x270B) + ": This round goes to you!";
         return "player";
     }
     else {
